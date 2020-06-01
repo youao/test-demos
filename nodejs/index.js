f53db7171f904b3b6cdaa5107ee06d8aa5b4eced
youao <aodada2588@163.com>
2020-06-01 22:00:37 +0800

    add ajax

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,6 +3,7 @@ const sever = require('koa-static');
 const router = require('koa-router')();
 const { createEnv } = require('./static/js/nunjucksEnv');
 const { initDB, con } = require('./static/js/db');
+
 initDB();
 
 const app = new Koa();
@@ -22,10 +23,33 @@ router.get('/', async (ctx, next) => {
     ctx.response.body = env.render('index.html');
 });
 
-router.get('/admin', async (ctx, next) => {
-    const sql = 'SELECT * FROM `news`';
-    const [data] = await con().execute(sql);
+router.get(['/admin', '/admin/news'], async (ctx, next) => {
+    const { page = 1 } = ctx.query;
+    // const offset = (currentPage - 1) * PER_SIZE;
+    // const limit = PER_SIZE;
+    const sql = 'SELECT * FROM `news` LIMIT ?,?';
+    const [data] = await con().execute(sql, [(page - 1) * 5, 5]);
+
     ctx.response.body = env.render('admin.html', { data });
+});
+
+router.get('/admin/newsAdd', async (ctx, next) => {
+    ctx.response.body = env.render('addnews.html');
+});
+
+router.get('/admin/newsDel', async (ctx, next) => {
+    const { id } = ctx.query;
+    if (!id) {
+        return false;
+    }
+    const sql = 'DELETE FROM `news` where `id`=?';
+    const [res] = await con().execute(sql, [id]);
+    // console.log(res);
+    const status = res.affectedRows ? !0 : !1;
+    ctx.body = JSON.stringify({
+        status,
+        msg: '删除' + (status ? '成功' : '失败')
+    });
 });
 
 app.use(router.routes());
